1b8f8af7a64b6c8d499c69b6e1e53f4152d7d731
Alain Zachary M. Atienza <alainzacharym.atienza@gmail.com>
2025-11-07 16:50:53 +0800

    feat/list-species-catered

--- veterinary.sql
+++ veterinary.sql
@@ -141,3 +141,5 @@ WHERE animalid = (SELECT animalid FROM animals WHERE name='Simba');
 UPDATE doctors
 SET dlastname = 'Reyes-Gonzales'
 WHERE dfirstname='Dr.Sofia';
+
+SELECT DISTINCT species FROM animals;
